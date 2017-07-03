118cc915d0fb3b41bd9806c348b9a4db2b63dd7a
traverseda <traverse.da@gmail.com>
2017-07-03 15:26:47 -0300

    Fixed app name error.

--- Settings/defaultSettings.py
+++ Settings/defaultSettings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reversion'
+    'reversion',
 
     'cms',  # django CMS itself
     'treebeard',  # utilities for implementing a tree
